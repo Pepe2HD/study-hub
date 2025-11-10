1cc31d5dbf68fb75d82ee486a5d8582b8ddcdbe3
Pepe2HD <pedrohd977@gmail.com>
2025-11-10 15:29:39 -0300

    Update createDisc.js

--- ProjetoExtensionista/js/admin/createDisc.js
+++ ProjetoExtensionista/js/admin/createDisc.js
@@ -35,16 +35,6 @@ document.addEventListener('DOMContentLoaded', function () {
             window.location.href = '/html/admin/disciplina.html';
         });
     }
-
-    const menuBtn = document.getElementById('menu-btn');
-    const sidebar = document.getElementById('sidebar');
-
-    if (menuBtn && sidebar) {
-        menuBtn.addEventListener('click', function () {
-            sidebar.classList.toggle('active');
-        });
-    }
-
 });
 
 const btnAbrirModalCurso = document.getElementById('btnAbrirModalCurso');
@@ -174,5 +164,6 @@ btnCadastrar.addEventListener('click', () => {
     cursosAssociadosTags.innerHTML = '';
     updateButtonStatus();
 });
+
 
 updateButtonStatus();

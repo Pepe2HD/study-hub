bfb02044768f6a5e61bdf3c1ad540c4c09224779
Pepe2HD <pedrohd977@gmail.com>
2025-11-10 15:30:09 -0300

    Update createProf.js

--- ProjetoExtensionista/js/admin/createProf.js
+++ ProjetoExtensionista/js/admin/createProf.js
@@ -36,13 +36,4 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    const menuBtn = document.getElementById('menu-btn');
-    const sidebar = document.getElementById('sidebar');
-
-    if (menuBtn && sidebar) {
-        menuBtn.addEventListener('click', function () {
-            sidebar.classList.toggle('active');
-        });
-    }
-
 });

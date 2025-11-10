9660481801dfa95c1e182278bbfb54179e1da4cc
Pepe2HD <pedrohd977@gmail.com>
2025-11-10 15:31:58 -0300

    Update createSala.js

--- ProjetoExtensionista/js/admin/createSala.js
+++ ProjetoExtensionista/js/admin/createSala.js
@@ -44,13 +44,4 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    const menuBtn = document.getElementById('menu-btn');
-    const sidebar = document.getElementById('sidebar');
-
-    if (menuBtn && sidebar) {
-        menuBtn.addEventListener('click', function () {
-            sidebar.classList.toggle('active');
-        });
-    }
-
 });

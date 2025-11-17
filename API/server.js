deb55047ee6e38617eb54fce8e1b2ee166cf2bef
Pepe2HD <pedrohd977@gmail.com>
2025-11-17 19:05:40 -0300

    Update server.js

--- API/server.js
+++ API/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const cors = require('cors'); // Importa o cors
 const app = express();
-const routes = require('./routes'); 
+const routes = require('./routes');
+
+app.use(cors({
+    origin: 'http://127.0.0.1:5500', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type']
+}));
 
 app.use(express.json());
 app.use(routes);
 
 app.listen(3000, () => {
     console.log("Server em funcionamento.");
-})
-
+});

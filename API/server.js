20a28c671235e5b30543aa6f87aa3f4dcc778fba
PedroV <165428109+PedroVinLS@users.noreply.github.com>
2025-11-17 20:35:44 -0300

    Update server.js

--- API/server.js
+++ API/server.js
@@ -4,7 +4,7 @@ const app = express();
 const routes = require('./routes');
 
 app.use(cors({
-    origin: 'http://127.0.0.1:5500', 
+    origin: ['http://127.0.0.1:5500','https://studys-hub.netlify.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type']
 }));

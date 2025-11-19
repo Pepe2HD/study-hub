54376a7cf28b02f29c2627e67e45879fe62515cf
PedroV <165428109+PedroVinLS@users.noreply.github.com>
2025-11-19 11:30:10 -0300

    Update periodo.js

--- API/models/periodo.js
+++ API/models/periodo.js
@@ -10,9 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.hasMany(models.Disciplina, {
+      this.hasMany(models.Horario, {
         foreignKey: 'id_periodo' ,
-        as: 'disciplina',
+        as: 'horario',
       })
     }
   }

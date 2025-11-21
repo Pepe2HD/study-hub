97ceaf6764da8e853b56e4fd74f6ffe8e0c0e743
PedroV <165428109+PedroVinLS@users.noreply.github.com>
2025-11-21 20:02:38 -0300

    Add login and reset token fields to admin table

--- API/migrations/20251109210323-create-admin.js
+++ API/migrations/20251109210323-create-admin.js
@@ -22,6 +22,22 @@ module.exports = {
         type: Sequelize.STRING(255),
         allowNull: false,
       },
+      login_code: {
+        type: Sequelize.STRING(6),
+        allowNull: true,
+      },
+      login_expires: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      reset_token: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      reset_expires: {
+        type: Sequelize.DATE,
+        allowNull: true
+      }
     });
   },
   async down(queryInterface, Sequelize) {

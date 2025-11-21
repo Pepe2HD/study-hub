c64bb036a3f4f1c59bb0a6f549b10b9888eed56c
PedroV <165428109+PedroVinLS@users.noreply.github.com>
2025-11-21 19:35:53 -0300

    Add new origin to CORS configuration

--- API/server.js
+++ API/server.js
@@ -6,6 +6,7 @@ const routes = require('./routes');
 app.use(cors({
   origin: [
     "https://study-hubs.netlify.app",
+    "https://study-hub-pearl.vercel.app",
     "http://127.0.0.1:5500"         
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 

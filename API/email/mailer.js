853fab67434418996f3c402887ebfdfa91dbe5bb
PedroV <165428109+PedroVinLS@users.noreply.github.com>
2025-11-21 23:10:08 -0300

    Update SMTP configuration for Gmail transport

--- API/email/mailer.js
+++ API/email/mailer.js
@@ -2,11 +2,18 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,       // 587 para STARTTLS
+    secure: false,   // STARTTLS
     auth: {
-        user: process.env.EMAIL_USER, 
-        pass: process.env.EMAIL_PASS 
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     }
 });
+
+transporter.verify((error, success) => {
+    console.log(error || "SMTP conectado!");
+});
+
 
 module.exports = transporter;
